ab0fc9ecbff0051a730fbca6c0ef97d4e62ac419
AstolfoTheDev <ropankatz@gmail.com>
2026-08-28 13:17:23 +0200

    uploaded baremetal src files

---
+++ dse/baremetal_src/fft_benchmark.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include "fftw3.h"    // The FFTW API
+#include "printf.h"
+#include "printf.c"   
+#include "syscalls.c" 
+
+#define UART_BASE 0x10020000
+#define UART_TXDATA (UART_BASE + 0x00)
+#define UART_TXCTRL (UART_BASE + 0x08)
+
+void uart_init() {
+    volatile int *txctrl = (int*)UART_TXCTRL;
+    *txctrl = 1; 
+}
+
+void _putchar(char c) {
+    volatile int *txdata = (int*)UART_TXDATA;
+    while ((*txdata) & (1 << 31)); // Wait for buffer
+    *txdata = (int)c;
+}
+
+int main() {
+    uart_init();
+    
+    // Checkpoint 1: Did we even boot?
+    //printf("1. BOOT\n");
+    
+    int N = 16; 
+    fftw_complex *in, *out;
+    fftw_plan p;
+
+    // Checkpoint 2: Did malloc survive?
+    //printf("2. MALLOC\n");
+    in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+
+    if (!in || !out) {
+        printf("FATAL: Out of memory!\n");
+        while(1){}
+    }
+
+    for (int i = 0; i < N; i++) {
+        in[i][0] = i; 
+        in[i][1] = 0; 
+    }
+
+    // Checkpoint 3: Does the Plan crash the Stack?
+    //printf("3. PLAN\n");
+    p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    // Checkpoint 4: Does the Execution crash the FPU?
+    //printf("4. EXECUTE\n");
+    fftw_execute(p);
+
+    printf("\nFFT Results\n");
+    for(int i = 0; i < 4; i++) {
+        // We cast to int for bare-metal printing safety
+        printf("Bin[%d]: Real: %d, Imag: %d\n", i, (int)out[i][0], (int)out[i][1]);
+    }
+
+
+    //printf("5. SUCCESS!\n");
+
+    // CRITICAL: DO NOT REMOVE THIS LOOP
+    for (volatile int i = 0; i < 2000000; i++);
+    
+    return 0;
+}
